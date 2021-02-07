7e56f8cb42b124eeb4316efc91b2a6a2a34b4856
Pablo <37776542+pablomptc@users.noreply.github.com>
2021-02-07 18:06:14 +0100

    Update Asociación2.py

--- Asociación2.py
+++ Asociación2.py
@@ -1,10 +1,23 @@
+#Libraries
 import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
-df = pd.read_csv ("Asociación.csv")
+"""
+The csv that is introduced has to have a specific format. The columns will be the different products.
+Each row will be each purchase, and True if the product is buyed; false if it's not buyed.
+
+    Pizza   Beer   Napkins   Avocado    Deodorant   ...
+ 1  True    False   False     False      False
+ 2  False   True    False     True       True
+ 3  False   False   True      False      False
+ 4  True    True    False     False      True
+ ...
+ 
+"""
+df = pd.read_csv ("association_input.csv")
 
 frequent_itemsets = apriori(df > 0, min_support = 0.06, use_colnames = True)
 rules = association_rules (frequent_itemsets, metric = "confidence", min_threshold = 0.8)
 rules = association_rules (frequent_itemsets, metric = "lift", min_threshold = 1)
-rules.to_csv ("asociation.csv")
+rules.to_csv ("association_output.csv")
